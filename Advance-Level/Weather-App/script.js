0988c37758b1dabc490a15c1ba9d02523a6a84c1
Mr-RitikSingh <ritiksingh-06>
2024-10-08 00:05:45 +0530

    weather app html @ css completed without media quary

---
+++ Advance-Level/Weather-App/script.js
@@ -0,0 +1,14 @@
+// // Check if Geolocation is supported
+// if ("geolocation" in navigator) {
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//         // Success callback
+//         const latitude = position.coords.latitude;
+//         const longitude = position.coords.longitude;
+//         console.log("Latitude:", latitude, "Longitude:", longitude);
+//     }, function(error) {
+//         // Error callback
+//         console.error("Error occurred while fetching location:", error);
+//     });
+// } else {
+//     console.error("Geolocation is not supported by this browser.");
+// }
